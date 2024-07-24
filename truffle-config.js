703c28c1ce1222e4de238c3e38709d4a110c7af6
v charan raju <96484862+VCharanRaju12@users.noreply.github.com>
2024-07-24 22:45:50 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
-const privateKeys = process.env.PRIVATE_KEYS || ""
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 module.exports = {
   networks: {
@@ -11,22 +11,21 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(
-          privateKeys.split(','), // Array of account private keys
-          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
-        )
-      },
+    mumbai: {
+      provider: () => new HDWalletProvider(
+        privateKeys.split(','), // Array of account private keys
+        `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}` // Infura URL for Polygon Mumbai
+      ),
       gas: 5000000,
       gasPrice: 25000000000,
-      network_id: 3
+      network_id: 2442 // Polygon Mumbai testnet network ID
     }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
     solc: {
+      version: "0.8.0", // Specify the solc version you are using
       optimizer: {
         enabled: true,
         runs: 200
